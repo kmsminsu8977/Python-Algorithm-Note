f1c3c176067777e48b1abb60ab8fdf417fc61c5a
kmsminsu8977 <kmsminsu8977@gmail.com>
2021-04-14 01:26:49 +0000

    Update Sort

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,13 +1,11 @@
 n = 5
 data = [8, 5, 4, 7, 2]
 
-''' Selection Sort '''
 for i in range(n):
-    min_index = i # The index of the minimum value.
-    for j in range(i + 1, n):
+    min_index = i
+    for j in range(i+1,n):
         if data[min_index] > data[j]:
             min_index = j
-    data[i], data[min_index] = data[min_index], data[i] # Swap.
+    data[i], data[min_index] = data[min_index], data[i]
 
-for x in data:
-    print(x)
+print(data)
