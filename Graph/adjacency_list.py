5ff0fff3c20afd9c32df5476c019fe695026ac26
kmsminsu8977 <kmsminsu8977@gmail.com>
2021-04-16 01:58:20 +0000

    Update Graph

--- Graph/adjacency_list.py
+++ Graph/adjacency_list.py
@@ -1,1 +1,32 @@
+graph = []
+
+# Graph Creation
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+print(graph)
+
+# Graph Initialization
+n, m = map(int,input().split()) # 9 3
+graph = [[0]*m for _ in range(n)]
+print(graph)
+
+# Graph Input
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))   # list(map(int,input()))
+print(graph)
+
+
+
+
+
 
